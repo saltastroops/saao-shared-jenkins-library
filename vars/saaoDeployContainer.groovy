72a68bc0575b8d798f0b9bfa606e7f9e5681bdd1
Christian Hettlage <hettlage@saao.ac.za>
2023-05-08 19:19:35 +0200

    Implement the function

--- vars/saaoDeployContainer.groovy
+++ vars/saaoDeployContainer.groovy
@@ -1,7 +1,8 @@
-def call(Map config = [:]) {
+def call(Map config = [:])
+{
   // Ensure all required arguments are given
   String[] arguments = ["host", "hostCredentialsId", "registryCredentialsId", "registryUrl"]
-  for (String argument: arguments) {
+  for (String argument : arguments) {
     if (!config.containsKey(argument)) {
       error "The ${argument} argument is missing."
     }
@@ -10,8 +11,14 @@ def call(Map config = [:]) {
   // Use the short git hash as the image tag
   def tag = sh returnStdout: true, script: 'git rev-parse --short HEAD | tr -d "\n"'
 
-  // Get the credentials
-  withCredentials([usernamePassword(credentialsId: config.registryCredentialsId, passwordVariable: 'registryPassword', usernameVariable: 'registryUsername')]) {
+  withCredentials([usernamePassword(credentialsId: config.registryCredentialsId, passwordVariable: 'registryPassword',
+                                    usernameVariable: 'registryUsername')]) {
+    // Build the Docker image
     dockerImage = docker.build("${registryUsername}/finder-chart-generator:${tag}")
+
+    // Push the image to the container registry
+    docker.withRegistry(config.registryUrl, config.registryCredentialsId) {
+      dockerImage.push()
+    }
   }
 }
