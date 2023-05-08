ebad26a19770c3277fd035506d269981b6eecf05
Christian Hettlage <hettlage@saao.ac.za>
2023-05-08 18:53:39 +0200

    Check the arguments

--- vars/saaoDeployContainer.groovy
+++ vars/saaoDeployContainer.groovy
@@ -1,4 +1,12 @@
 def call(Map config = [:]) {
+  // Ensure all required arguments are given
+  String[] arguments = ["host", "hostCredentials", "registryCredentialsId", "registryUrl"]
+  for (String argument: arguments) {
+    if (!config.containsKey(argument)) {
+      error "The registryCredentialsId argument is missing."
+    }
+  }
+
   _deployToRegistry()
 }
 
