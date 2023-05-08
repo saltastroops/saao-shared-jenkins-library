11fec99dd9f19431c1059b3afb7931c601c8012f
Christian Hettlage <hettlage@saao.ac.za>
2023-05-08 19:28:28 +0200

    Implement the function

--- vars/saaoDeployContainer.groovy
+++ vars/saaoDeployContainer.groovy
@@ -1,7 +1,11 @@
 def call(Map config = [:])
 {
   // Ensure all required arguments are given
-  String[] arguments = ["host", "hostCredentialsId", "registryCredentialsId", "registryUrl"]
+  String[] arguments = ["host",
+                        "hostCredentialsId",
+                        "hostDir",
+                        "registryCredentialsId",
+                        "registryUrl"]
   for (String argument : arguments) {
     if (!config.containsKey(argument)) {
       error "The ${argument} argument is missing."
@@ -11,8 +15,12 @@ def call(Map config = [:])
   // Use the short git hash as the image tag
   def tag = sh returnStdout: true, script: 'git rev-parse --short HEAD | tr -d "\n"'
 
-  withCredentials([usernamePassword(credentialsId: config.registryCredentialsId, passwordVariable: 'registryPassword',
-                                    usernameVariable: 'registryUsername')]) {
+  withCredentials([usernamePassword(credentialsId: config.registryCredentialsId,
+                                    passwordVariable: 'registryPassword',
+                                    usernameVariable: 'registryUsername'),
+                   sshUserPrivateKey(credentialsId: config.hostCredentialsId,
+                                     keyFileVariable: 'identity',
+                                     usernameVariable: 'username')]) {
     // Build the Docker image
     dockerImage = docker.build("${registryUsername}/finder-chart-generator:${tag}")
 
@@ -20,5 +28,17 @@ def call(Map config = [:])
     docker.withRegistry(config.registryUrl, config.registryCredentialsId) {
       dockerImage.push()
     }
+
+    // Define the remote connection parameters
+    def remote = [:]
+    remote.name = config.host
+    remote.host = config.host
+    remote.allowAnyHosts = true
+    remote.user = username
+    remote.identityFile = identity
+
+    // Create the necessary remote directory. The -p flag is used to avoid an
+    // error if the directory exists already.
+    sshCommand remote: remote, command: "mkdir -p ${config.hostDir}"
   }
 }
