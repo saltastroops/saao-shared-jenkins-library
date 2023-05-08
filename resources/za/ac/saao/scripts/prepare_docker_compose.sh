852600869643797000e8f46a5e28034574ea3bc6
Christian Hettlage <hettlage@saao.ac.za>
2023-05-08 21:33:36 +0200

    Implement the function

--- resources/za/ac/saao/scripts/prepare_docker_compose.sh
+++ resources/za/ac/saao/scripts/prepare_docker_compose.sh
@@ -4,16 +4,16 @@
 #
 # The following arguments need to be passed to the script, in this order:
 #
-# 1. The container registry URLm such as https://registry.example.com.
+# 1. The container registry URL such as https://registry.example.com.
 # 2. The container registry username.
 # 3. The Docker image name, such as my-great-webapp.
 # 4. The Docker image tag.
 
 # The docker registry variable should have an "https://" or "http://" prefix, but this
 # must not be included in the pushed image name.
-registry=$(echo ${REGISTRY} | sed s#https://## | sed s#http://##)
+registry=$(echo "$1" | sed s#https://## | sed s#http://##)
 
-# Replace the ${DOCKER_REGISTRY}, ${DOCKER_REGISTRY_USERNAME} and ${TAG} environment
+# Replace the ${REGISTRY}, ${REGISTRY_USERNAME}, ${IMAGE_NAME} and ${TAG} environment
 # variables in the docker compose file with the correct values, and output the result to
 # stdout.
 # Note that the pound sign is used as the delimiter for the first sed pattern, as the
@@ -21,9 +21,9 @@ registry=$(echo ${REGISTRY} | sed s#https://## | sed s#http://##)
 registry_pattern='s#${REGISTRY}#'
 registry_pattern+="${registry}#"
 username_pattern='s/${REGISTRY_USERNAME}/'
-username_pattern+="${REGISTRY_CREDENTIALS_USR}/"
+username_pattern+="$2/"
 image_name_pattern='s/${IMAGE_NAME}/'
-image_name_pattern+=""
+image_name_pattern+="$3/"
 tag_pattern='s/${TAG}/'
-tag_pattern+="${TAG}/"
-sed "$registry_pattern" docker-compose.yml | sed "$username_pattern" | sed "$tag_pattern"
+tag_pattern+="$4/"
+sed "$registry_pattern" docker-compose.yml | sed "$username_pattern" | sed "$image_name_pattern" | sed "$tag_pattern"
