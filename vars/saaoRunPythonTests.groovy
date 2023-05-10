8005f1c1b40f3b16b91cdad11f7810b03a54070f
Christian Hettlage <hettlage@saao.ac.za>
2023-05-10 21:34:17 +0200

    Implement the function

--- vars/saaoRunPythonTests.groovy
+++ vars/saaoRunPythonTests.groovy
@@ -1,14 +1,27 @@
 def call(Map config = [:] ) {
-  def success = true
+  // Get the directories to test
   def banditDirs = _dirs(config, "bandit")
+  def blackDirs = _dirs(config, "black")
   def flake8Dirs = _dirs(config, "flake8")
-  echo banditDirs.toString()
-  echo flake8Dirs.toString()
+  def isortDirs = _dirs(config, "isort")
+  def mypyDirs = _dirs(config, "mypy")
+  def pytestDirs = _dirs(config, "pytest")
+
+  // Run the various tests
+  def success = false
+  if (banditDirs.length() > 0) {
+    returnValue = sh 'returnStatus': true, 'script': "bandit -r $banditDirs"
+    if (returnValue != 0) {
+      echo "bandit failed."
+      success = false
+    }
+  }
+  return success
 }
 
 def _dirs(Map config, String key) {
   if (config.containsKey(key)) {
-    return config.get(key)
+    return config.get(key).join(' ')
   }
-  return []
+  return ''
 }
