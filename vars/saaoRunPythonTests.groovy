70f2946fb6226cf7f96028ad2230d5f7c5dd96f4
Christian Hettlage <hettlage@saao.ac.za>
2023-05-10 21:16:36 +0200

    Implement the function

--- vars/saaoRunPythonTests.groovy
+++ vars/saaoRunPythonTests.groovy
@@ -1,3 +1,13 @@
-def call() {
-  sh 'black --check fcg tests'
+def call(Map config = [:] ) {
+  def success = true
+  def banditDirs = _dirs(config, "bandit")
+  def flake8Dirs = _dirs(config, "flake8")
+  echo banditDirs
+}
+
+def _dirs(Map config, String key) {
+  if (config.containsKey(key)) {
+    return config.get(key)
+  }
+  return []
 }
